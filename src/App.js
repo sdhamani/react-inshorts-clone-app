4a6c3fad561c4bccc21dce4a95ec27f23cc2e3d2
sdhamani <74397480+sdhamani@users.noreply.github.com>
2021-02-08 22:17:29 +0530

    changed api link

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function App() {
 
         const proxyUrl = "https://cors-anywhere.herokuapp.com/";
         console.log(loadMore)
-        const news = await axios.get(`${proxyUrl}https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${loadMore}`)
+        // const news = await axios.get(`${proxyUrl}https://newsapi.org/v2/top-headlines?country=in&apiKey=${apikey}&category=${category}&pageSize=${loadMore}`)
+        const news = await axios.get(`https://saurav.tech/NewsAPI/top-headlines/category/general/in.json`)
         setnewsArray(news.data.articles)
         setnewsResults(news.data.totalResults)
       }
